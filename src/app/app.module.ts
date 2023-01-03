c7c0d1cfc12c20c60b5fe852fa01db43ad0b3ea5
KingRalph <rgradison@gmail.com>
2023-01-03 14:13:59 +0200

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {NotesComponent} from './notes/notes.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
+const appRoutes : Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    //Default route
+    path:'',
+    component:NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NotesComponent,
+    NotFoundComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
